88c704e28068b0c2fbd8f6d5127309b418805f8a
TheDarkPoopVadeee <alejandro.sesti.diaz@outlook.com>
2024-08-26 19:07:24 -0500

    Ejercicio de perfil de usuario completado

---
+++ TALLERES/TALLER_2/perfil_usuario.php
@@ -0,0 +1,20 @@
+<?php
+    $nombre_completo="ALEJANDRO SESTI";
+    $edad=27;
+    $correo="alejandro.sesti.diaz@utp.ac.pa";
+    $telefono= 64968908;
+    define("OCUPACION", "estudiante");
+
+    $presentacion1 = "Hola, mi nombre es " . $nombre_completo . " y tengo " . $edad . " años.";
+    $presentacion2 = "Con correo". $correo . "y numero celular". $telefono . "ocupacion". OCUPACION;
+
+    echo "<br>Información de debugging:<br>";
+    var_dump($nombre_completo);
+    echo "<br>";
+    var_dump($edad);
+    echo "<br>";
+    var_dump($correo);
+    echo "<br>";
+    var_dump(OCUPACION);
+    echo "<br>";
+?>
